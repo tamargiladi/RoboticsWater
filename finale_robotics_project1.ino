bc981ed7d8406c1c82b8049095306be9ce6c6933
Ran Ben Melech <ran.ben.melech1@gmail.com>
2020-12-27 16:54:01 +0200

    Add files via upload

--- finale_robotics_project1.ino
+++ finale_robotics_project1.ino
@@ -1,13 +1,175 @@
 #include <DHT.h>
 #define Type DHT11
-#define RELAY 3 
+#define RELAY 9
+const int s1 = 10;
+const int s2 = 11;
+const int s3 = 12;
+const int s4 = 13;
+int motorSpeed = 1500; // motor speed between 1000-4000. small number is bigger speed 
+void openTap() 
+{
+  int watring_time=50000;
+  digitalWrite(RELAY, HIGH);
+  delay(watring_time);
+  digitalWrite(RELAY, LOW);
+  return;
+}
+void motorOff(){ // turn off motor
+ digitalWrite(s1, LOW); 
+ digitalWrite(s2, LOW); 
+ digitalWrite(s3, LOW); 
+ digitalWrite(s4, LOW); 
+}
 
-int sensePin=2;
+void back(){ // function to move 1 step back
+  digitalWrite(s1, HIGH); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, HIGH); 
+  digitalWrite(s2, HIGH); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, HIGH); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, HIGH); 
+  digitalWrite(s3, HIGH); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, HIGH); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, HIGH); 
+  digitalWrite(s4, HIGH); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, HIGH); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, HIGH); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, HIGH); 
+  delayMicroseconds(motorSpeed);
+}
+
+void forward(){ // function to move 1 step forward
+  digitalWrite(s1, HIGH); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, HIGH); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, HIGH); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, HIGH); 
+  digitalWrite(s4, HIGH); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, HIGH); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, HIGH); 
+  digitalWrite(s3, HIGH); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, LOW); 
+  digitalWrite(s2, HIGH); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, HIGH); 
+  digitalWrite(s2, HIGH); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+  digitalWrite(s1, HIGH); 
+  digitalWrite(s2, LOW); 
+  digitalWrite(s3, LOW); 
+  digitalWrite(s4, LOW); 
+  delayMicroseconds(motorSpeed);
+}
+void openLeftTap()
+{
+  for (int i=0; i<=150; i++){ // go forward one circle, 509 steps
+    back();
+  }
+  for (int i=0; i<=150; i++){ // go back one circle. 
+    forward();
+ }
+ return;
+}
+void left_close_or_open(int res)
+{
+  if (res==1||res==0)
+  {
+    //close left tap
+  }
+  if (res==2)
+  {
+    //open right
+  }
+}
+
+int day_or_night1(int light_sensor)
+{
+  if(light_sensor<300)
+  {
+    return(0);
+  }
+  else
+  {
+    return (1);
+  }
+}
+int checkStatus(int leftHumidity,int rightHumdity,int light_sensor)
+{
+  int day_or_night=day_or_night1(light_sensor);
+  //res possible outcomes:
+  //0 - dont open taps
+  //1 - open left tap
+  //2 - open both taps
+  int res=0;
+  //sensor1=left tap
+  //sensor2=right tap
+  if (day_or_night==0)
+  {
+    if (leftHumidity>400 && rightHumdity>400)
+    {
+      res=2;
+    }
+    else if (leftHumidity>400 && rightHumdity<400)
+    {
+      res=1;
+    }
+  }
+  return (res);
+}
+int sensePin=7;
 DHT HT(sensePin,Type);
 float humidity;
 float tempC;
 float tempF;
+int day_or_night;
 float res[5];
+
 char *sensorName[]={"humidity:","temperature:","first ground moisture:","second ground moisture:","light:"};
 int sensorPin = A0; // select the input pin for LDR
 int sensorPin1 = A1;
@@ -21,15 +183,13 @@ void getValuesToArr(DHT HT,int sensorPin,int sensorPin1,int sensorPin2,float res
   humidity=HT.readHumidity();
   tempC=HT.readTemperature();
   sensorValue = analogRead(sensorPin); // read the value from the sensor
-  sensorValue = map(sensorValue,550,0,0,100);
   sensorValue1 = analogRead(sensorPin1); // read the value from the sensor
-  sensorValue1 = map(sensorValue1,550,0,0,100);
   sensorValue2=analogRead(sensorPin2);
   res[0]=humidity;
   res[1]=tempC;
-  res[2]=sensorValue;//first ground moisture sensor
-  res[3]=sensorValue1;//second ground moisture sensor
-  res[4]=sensorValue2;//light sensor
+  res[2]=sensorValue1;//first ground moisture sensor
+  res[3]=sensorValue2;//second ground moisture sensor
+  res[4]=sensorValue;//light sensor
   return;
 }
 
@@ -39,20 +199,23 @@ void setup()
   pinMode(RELAY, OUTPUT);
   HT.begin();
   delay(500);
+  pinMode(s1, OUTPUT); 
+ pinMode(s2, OUTPUT); 
+ pinMode(s3, OUTPUT); 
+ pinMode(s4, OUTPUT); 
 }
 void loop() 
 {
   humidity=HT.readHumidity();
   tempC=HT.readTemperature();
   sensorValue = analogRead(sensorPin); // read the value from the sensor
-  sensorValue = map(sensorValue,550,0,0,100);
   sensorValue1=analogRead(sensorPin1);
-  sensorValue1=map(sensorValue1,550,0,0,100);
   sensorValue2=analogRead(sensorPin2);
   getValuesToArr(HT,sensorPin,sensorPin1,sensorPin2,res);
-  digitalWrite(RELAY, HIGH);
-  delay(3000);
+  openLeftTap();
   digitalWrite(RELAY, LOW);
+  delay(3000);
+  digitalWrite(RELAY, HIGH);
   delay(3000);
 
   int i=0;
@@ -61,4 +224,5 @@ void loop()
     Serial.print(sensorName[i]);
     Serial.println(res[i]);
   }
+  
 }
